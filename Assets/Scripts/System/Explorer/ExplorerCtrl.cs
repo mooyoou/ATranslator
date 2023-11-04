8f9c136d26e4448c519291742f26e817a2cb4d59
DESKTOP-I1NC1HA\Moyou <297110247@qq.com>
2023-11-04 23:45:35 +0800

    无限列表初步完成，下一步滚动更新节点

--- Assets/Scripts/System/Explorer/ExplorerCtrl.cs
+++ Assets/Scripts/System/Explorer/ExplorerCtrl.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Explorer;
 using System.IO;
 using System.Threading.Tasks;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.UI;
 using Utility;
@@ -20,6 +21,8 @@ public class ExplorerNode
     public ExplorerNode FatherExplorerNode;
     public FileInfo FileInfo;
 
+    public bool IsExpand;
+    
     public ExplorerNode(string fullPath,bool isFolder, int depth = 0,ExplorerNode fnode = null)
     {
         if (!Directory.Exists(fullPath) && !File.Exists(fullPath))
@@ -49,16 +52,13 @@ public class ExplorerNode
         string[] subdirectoryEntries = Directory.GetDirectories(FullPath);
         foreach (string subdirectory in subdirectoryEntries)
         {
-            Task.Run(() =>
-            {
-                SubExplorerNodes.Add(new ExplorerNode(subdirectory,true,Depth+1,this));
-            });
-
+            SubExplorerNodes.Add(new ExplorerNode(subdirectory,true,Depth+1,this));
         }
         foreach (string fileEntry in fileEntries)
         {
             SubExplorerNodes.Add(new ExplorerNode(fileEntry,false,Depth+1,this));
         }
+        
     }
 }
 
@@ -67,22 +67,23 @@ public class ExplorerCtrl : MonoBehaviour
     [SerializeField] 
     private Transform explorerNodeRoot;
     [SerializeField] 
-    private ExplorerNodeBtn explorerNodeP;
+    private ExplorerNodeBtn_fix explorerNodeP;
     [SerializeField] 
-    private ScrollRect scrollRect;
+    private ExplorerScrollRect explorerScrollRect;
     
-    private ExplorerNodeBtn _rootExplorerNodeBtn;
-    private ExplorerNodeBtn _oldRootExplorerNodeBtn;
+    
     private ExplorerNode _rootExplorerNode;
     private int _tipId;
-        
+    private string _lastChoosePath;
     private Coroutine _initNodesCoroutine;
+
+    private List<ExplorerNodeBtn_fix> InitBtnList = new List<ExplorerNodeBtn_fix>();
     private void Awake()
     {
         RegisterEvent();
     }
 
-    public void RegisterEvent()
+    private void RegisterEvent()
     {
         GlobalSubscribeSys.Subscribe("open_new_project", (objects) =>
         {
@@ -91,23 +92,137 @@ public class ExplorerCtrl : MonoBehaviour
         GlobalSubscribeSys.Subscribe("cancel_explorer_init", (objects) =>
         {
             StopCoroutine(_initNodesCoroutine);
-            if (_oldRootExplorerNodeBtn != null && _oldRootExplorerNodeBtn != _rootExplorerNodeBtn)
-            {
-                Destroy(_rootExplorerNodeBtn.gameObject);
-                _rootExplorerNodeBtn = _oldRootExplorerNodeBtn;
-            }
-
+            ClearOldNodeBtn();
         });
+
     }
     
+    
+    /// <summary>
+    /// 清除旧列表
+    /// </summary>
+    private void ClearOldNodeBtn()
+    {
+
+    }
     /// <summary>
     /// 顶部菜单栏-打开（新项目）
     /// </summary>
     private void OpenNewProject()
     {
-        string folderPath = Misc.OpenFolderBrowserDialog(_rootExplorerNodeBtn!=null?_rootExplorerNode.FullPath:null);
-        if(string.IsNullOrEmpty(folderPath)) return;
+        //选择路径
+        _lastChoosePath = Misc.OpenFolderBrowserDialog(_lastChoosePath);
+        if(string.IsNullOrEmpty(_lastChoosePath)) return;
+
+        OpenTip(_lastChoosePath);
+        _initNodesCoroutine = StartCoroutine(InitNodesAsync(_lastChoosePath));
+
+    }
+
+    private  IEnumerator InitNodesAsync(string rootPath)
+    {
+        Task InitTreeTask = Task.Run(() =>
+        {
+            _rootExplorerNode = new ExplorerNode(rootPath,true,0,null); //建立文件结构树
+        });
+        yield return new WaitUntil(() => InitTreeTask.IsCompleted);
         
+        if (_rootExplorerNode == null)yield break ;
+        InitBtns(_rootExplorerNode);
+        CloseTip();
+    }
+
+
+    private void InitBtns(ExplorerNode rootNode)
+    {
+        explorerScrollRect.verticalScrollbar.value=0;
+        var explorerNodeBtn = Instantiate(explorerNodeP, explorerNodeRoot);
+        explorerNodeBtn.Init(rootNode,this);
+        InitBtnList.Add(explorerNodeBtn);
+    }
+
+    private void UpdateBtns()
+    {
+        Stack<ExplorerNode> nodeStack = new Stack<ExplorerNode>();
+        nodeStack.Push(_rootExplorerNode);
+        int newNodeNum = 0;
+        int allNodeNum = 0;
+        while (nodeStack.Count > 0)
+        {
+            ExplorerNode curNode = nodeStack.Pop();
+            allNodeNum++;
+            
+            if (curNode.IsFolder && curNode.IsExpand)
+            {
+                foreach (var nodeBtn in curNode.SubExplorerNodes)
+                {
+                    nodeStack.Push(nodeBtn);
+                }
+            }
+            
+            if (false)
+            {
+                //计算跳过的节点
+            }
+            
+            
+            if (newNodeNum < InitBtnList.Count)
+            {
+                InitBtnList[newNodeNum].gameObject.SetActive(true);
+                InitBtnList[newNodeNum].Init(curNode,this);
+                
+            }
+            else {
+                if (InitBtnList.Count > 50)
+                {
+                    continue;
+                }
+                //还可以生成新节点
+                var explorerNodeBtn = Instantiate(explorerNodeP, explorerNodeRoot);
+                explorerNodeBtn.Init(curNode,this);
+                InitBtnList.Add(explorerNodeBtn);
+            }
+            newNodeNum++;
+        }
+
+        if (newNodeNum < InitBtnList.Count)
+        {
+            for (int i = newNodeNum; i < InitBtnList.Count; i++)
+            {
+                InitBtnList[i].gameObject.SetActive(false);
+            }
+        }
+
+        explorerScrollRect.customVerticalSize = (float)newNodeNum / allNodeNum;
+
+    }
+    
+    public void ExpendBtn(ExplorerNode explorerNode)
+    {
+        float barValue = explorerScrollRect.verticalScrollbar.value;
+        float barSize = explorerScrollRect.verticalScrollbar.size;
+        UpdateBtns();
+    }
+    
+    /// <summary>
+    /// 关闭提示窗口
+    /// </summary>
+    private void CloseTip()
+    {
+        GlobalSubscribeSys.Invoke("close_tips_window",_tipId);
+        // if (_oldRootExplorerNodeBtn != null)
+        // {
+        //     Destroy(_oldRootExplorerNodeBtn.gameObject);
+        // }
+        // _oldRootExplorerNodeBtn = null;
+    }
+    
+    /// <summary>
+    /// 关闭提示窗口
+    /// </summary>
+    /// <param name="folderPath"></param>
+    private void OpenTip(string folderPath)
+    {
         GlobalSubscribeSys.Invoke("open_tips_window",out List<object> ids,new System.Object[]
         {
             "Project is opening",
@@ -116,27 +231,10 @@ public class ExplorerCtrl : MonoBehaviour
             "cancel_explorer_init"
         });
         _tipId = ids[0] as int? ?? 0;
-
-        _initNodesCoroutine = StartCoroutine(InitNodesAsync(folderPath));
     }
 
-    private IEnumerator InitNodesAsync(string rootPath)
-    {
-        //保底加载时间用于用户取消操作
-        yield return new WaitForSeconds(1);
-        _oldRootExplorerNodeBtn = _rootExplorerNodeBtn;
-        _rootExplorerNode = new ExplorerNode(rootPath,true,0,null);
-        if (_rootExplorerNode == null)yield break;
-        _rootExplorerNodeBtn = Instantiate(explorerNodeP, explorerNodeRoot);
-        _rootExplorerNodeBtn.Init(_rootExplorerNode,explorerNodeP);
-        GlobalSubscribeSys.Invoke("close_tips_window",_tipId);
-        if (_oldRootExplorerNodeBtn != null)
-        {
-            Destroy(_oldRootExplorerNodeBtn.gameObject);
-        }
-        _oldRootExplorerNodeBtn = null;
-    }
 
+    
     public void OnVerticalChange(Single value)
     {
         // var scrollRectTransform = scrollRect.transform;
---
+++ Assets/Scripts/System/Explorer/ExplorerNodeBtn_fix.cs
@@ -0,0 +1,73 @@
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.U2D;
+using UnityEngine.UI;
+
+namespace System.Explorer
+{
+    public class ExplorerNodeBtn_fix : MonoBehaviour
+    {
+        [SerializeField]
+        private Button extraBtn;
+        [SerializeField]
+        private Image fileIcon;
+        [SerializeField]
+        private TMP_Text tmpText;
+        [SerializeField] 
+        private SpriteAtlas explorerIcons;
+        [SerializeField] 
+        private HorizontalLayoutGroup horizontalLayoutGroup;
+
+        internal ExplorerNode ExplorerNode;
+        private ExplorerCtrl _explorerCtrl;
+        
+        private List<ExplorerNodeBtn> _explorerNodeBtns=new List<ExplorerNodeBtn>();
+
+        internal void Init(ExplorerNode explorerNode,ExplorerCtrl explorerCtrl)
+        {
+            ExplorerNode = explorerNode;
+            _explorerCtrl = explorerCtrl;
+            tmpText.text = ExplorerNode.FileName;
+            horizontalLayoutGroup.padding.left = explorerNode.Depth * 23;
+            if (ExplorerNode.IsFolder)
+            {
+                extraBtn.interactable = true;
+                fileIcon.sprite = explorerIcons.GetSprite("folder");
+            }
+            else
+            {
+                fileIcon.sprite = explorerIcons.GetSprite("file");
+                extraBtn.interactable = false;
+            }
+        }
+        /// <summary>
+        /// 展开按钮点击
+        /// </summary>
+        public void OnExpandClick()
+        {
+            if (ExplorerNode.IsFolder)
+            {
+                //状态处理
+                if (ExplorerNode.IsExpand)
+                {
+                    extraBtn.transform.rotation = Quaternion.Euler(0, 0, 0);
+                }
+                else
+                {
+                    extraBtn.transform.rotation = Quaternion.Euler(0, 0, -90);
+                }
+
+                //转变状态
+                ExplorerNode.IsExpand = !ExplorerNode.IsExpand;
+
+                _explorerCtrl.ExpendBtn(ExplorerNode);
+            }
+        }
+        public void OnNodeClick()
+        {
+
+        }
+
+    }
+}
